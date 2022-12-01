1b993351173395c6feff060db8061a9824e745ff
Mirko Franco <francomirko91@gmail.com>
2022-12-01 15:30:42 +0100

    class house completed

---
+++ src/it/thirdExercise/House.java
@@ -0,0 +1,13 @@
+package it.thirdExercise;
+
+public class House {
+    public String address;
+    public int numberOfFloors;
+
+
+    public House(String address,int numberOfFloors) {
+        System.out.println("we are creating the house");
+    }
+
+
+}
